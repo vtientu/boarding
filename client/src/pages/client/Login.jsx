a7d7b56920092c7854541e442ead03d79879ccba
danggianguyen <nguyendg@aladintech.vn>
2025-03-29 16:35:54 +0700

    fix: login

--- client/src/pages/client/Login.jsx
+++ client/src/pages/client/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
 			};
 			try {
 				const response = await axios.post(
-					"http://localhost:3000/api/v1/login",
+					"http://localhost:3000/users/login",
 					formData
 				);
 				localStorage.setItem("auth", JSON.stringify(response.data.token));
